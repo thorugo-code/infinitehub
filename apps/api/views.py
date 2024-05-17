6f3a3976867c6049bbd67c4ea23575057d561955
Vitor Hugo Lima <vitorhrflima@gmail.com>
2024-05-17 13:05:50 -0300

    fix: Work with body content

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,15 +1,23 @@
+import json
 from django.shortcuts import redirect
-from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def receive(request):
-    messages.info(request, 'You have successfully created a meeting')
-    print(request)
-    print(request.GET)
-    print(request.POST)
-    print(request.COOKIES)
-    print(request.FILES)
-    print(request.method)
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+
+    headers = {key: value for key, value in request.headers.items()}
+
+    post_params = request.POST.dict()
+
+    response_data = {
+        'body': body_data,
+        'headers': headers,
+        'params': post_params,
+    }
+
+    print(response_data)
+
     return redirect('home')
